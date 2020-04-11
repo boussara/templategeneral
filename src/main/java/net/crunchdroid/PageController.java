56a4583d565879ec71908922649971db8cee46df
Mirage <you@example.cominventoory@gmail.com>
2020-04-11 20:49:33 +0100

    chart.js

--- src/main/java/net/crunchdroid/PageController.java
+++ src/main/java/net/crunchdroid/PageController.java
@@ -1,7 +1,12 @@
 package net.crunchdroid;
 
+import java.util.LinkedHashMap;
+import java.util.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author CrunchDroid
@@ -9,17 +14,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-  
+    @RequestMapping(value = "/home", method=RequestMethod.GET)
+    public String chart(Model model) {
+         
+        //first, add the regional sales
+        Integer northeastSales = 17089;
+        Integer westSales = 10603;
+        Integer midwestSales = 5223;
+        Integer southSales = 10111;
+         
+        model.addAttribute("northeastSales", northeastSales);
+        model.addAttribute("southSales", southSales);
+        model.addAttribute("midwestSales", midwestSales);
+        model.addAttribute("westSales", westSales);
+         
+        //now add sales by lure type
+        List<Integer> inshoreSales = Arrays.asList(4074, 3455, 4112);
+        List<Integer> nearshoreSales = Arrays.asList(3222, 3011, 3788);
+        List<Integer> offshoreSales = Arrays.asList(7811, 7098, 6455);
+         
+        model.addAttribute("inshoreSales", inshoreSales);
+        model.addAttribute("nearshoreSales", nearshoreSales);
+        model.addAttribute("offshoreSales", offshoreSales);
+         
+        return "home";
+    }
+     
 
     @GetMapping("/")
     public String login() {
         return "login";
     }
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
-    }
+ 
 
     @GetMapping("/plain-page")
     public String plainPage() {
