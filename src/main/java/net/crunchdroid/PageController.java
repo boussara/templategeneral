f3107a7a3d20bc41b0c90a02e21db0aef10133c2
Mirage <you@example.cominventoory@gmail.com>
2020-04-12 16:04:09 +0100

    page error et messages fr

--- src/main/java/net/crunchdroid/PageController.java
+++ src/main/java/net/crunchdroid/PageController.java
@@ -1,12 +1,7 @@
 package net.crunchdroid;
 
-import java.util.LinkedHashMap;
-import java.util.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author CrunchDroid
@@ -14,29 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class PageController {
 
-    @RequestMapping(value = "/home", method=RequestMethod.GET)
-    public String chart(Model model) {
-         
-        //first, add the regional sales
-        Integer northeastSales = 17089;
-        Integer westSales = 10603;
-        Integer midwestSales = 5223;
-        Integer southSales = 10111;
-         
-        model.addAttribute("northeastSales", northeastSales);
-        model.addAttribute("southSales", southSales);
-        model.addAttribute("midwestSales", midwestSales);
-        model.addAttribute("westSales", westSales);
-         
-        //now add sales by lure type
-        List<Integer> inshoreSales = Arrays.asList(4074, 3455, 4112);
-        List<Integer> nearshoreSales = Arrays.asList(3222, 3011, 3788);
-        List<Integer> offshoreSales = Arrays.asList(7811, 7098, 6455);
-         
-        model.addAttribute("inshoreSales", inshoreSales);
-        model.addAttribute("nearshoreSales", nearshoreSales);
-        model.addAttribute("offshoreSales", offshoreSales);
-         
+    @GetMapping("/home")
+    public String home() {
         return "home";
     }
      
--- src/main/java/net/crunchdroid/ThymeleafWebMvcConfig.java
+++ src/main/java/net/crunchdroid/ThymeleafWebMvcConfig.java
@@ -1,10 +1,22 @@
 package net.crunchdroid;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.autoconfigure.web.WebMvcProperties.LocaleResolver;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
@@ -43,5 +55,44 @@ public class ThymeleafWebMvcConfig extends WebMvcConfigurerAdapter {
         return templateResolver;
     }
 
+    //***************************LANG********************************///
+    @Bean
+	public CookieLocaleResolver localeResolver() {
+		return new CookieLocaleResolver();
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("i18n");
+		registry.addInterceptor(localeChangeInterceptor);
+	}
+	
+	@Bean("messageSource")
+	public MessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasenames("i18n/messages");
+	    messageSource.setDefaultEncoding("UTF-8");
+	    return messageSource;
+	}
+//*********************Error**************************************///
 
+@RequestMapping("/error")
+public String handleError(HttpServletRequest request) {
+    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+    if (status != null) {
+        Integer statusCode = Integer.valueOf(status.toString());
+
+        if(statusCode == HttpStatus.NOT_FOUND.value()) {
+            return "error404";
+        }
+        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            return "error500";
+        }
+    }
+    return "error";
+}
+//***********************************************************///
+    
 }
